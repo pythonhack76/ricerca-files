4b99e534b3b3952a3fcc9f5926f04be15e4d8627
Python The Hack <lrulvoni@gmail.com>
2022-03-16 08:10:17 +0100

    update window

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from tkinter import *
 
+def esegui_ricerca():
+    pass
+
+def reset_ricerca():
+    pass
+
+def view_ricerca():
+    pass
+
+def chiudi_programma():
+    pass
+
+
 #creo la finestra per applicativo
 window = Tk()
 window.geometry("500x500")
+window.title("ricerca il tuo file")
 
-label= Label(tesxt = "Inserisci indirizzo cartella:")
+#creo titolo
+label0 = Label(text = "Cerca e trova Files")
+label0.place(x = 180, y = 20)
+#creo campo per ricerca file
+label= Label(text = "Inserisci indirizzo cartella:")
 label.place(x = 30, y = 40)
 label.config(bg='lightgreen', padx=10)
+
+ricerca = Entry(text = "")
+ricerca.place(x=190, y=40, width=200, height=20)
+
+#inserisco bottone per avvio ricerca
+button = Button(text = "ricerca", command = esegui_ricerca)
+button.place(x=200, y=80, width=75, height=35)
+
+
 
 
 
