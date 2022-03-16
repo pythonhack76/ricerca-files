1e3a30216f453a1723426c5c5b92b6d013e67f95
Python The Hack <lrulvoni@gmail.com>
2022-03-16 08:01:44 +0100

    update window

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+#creo la finestra per applicativo
+window = Tk()
+window.geometry("500x500")
+
+label= Label(tesxt = "Inserisci indirizzo cartella:")
+label.place(x = 30, y = 40)
+label.config(bg='lightgreen', padx=10)
+
+
+
+
+window.mainloop() 
