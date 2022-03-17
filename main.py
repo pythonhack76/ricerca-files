d555972db2644960bc2fa07d28b61a62cf308489
Python The Hack <lrulvoni@gmail.com>
2022-03-17 14:43:22 +0100

    update get risultati

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #un programma per cercare files in directory 
-
 from tkinter import *
+import tkinter as tk
+import os 
 
 def esegui_ricerca():
     pass
@@ -9,16 +10,19 @@ def reset_ricerca():
     pass
 
 def view_ricerca():
-    pass
+    view = ricerca.get()
+    print(view)
+    
 
-def chiudi_programma():
-    pass
 
 
 #creo la finestra per applicativo
 window = Tk()
 window.geometry("500x500")
 window.title("ricerca il tuo file")
+window.resizable(False, False)
+window.configure(background="lightblue")
+my_font=('times', 8, 'bold')
 
 #creo titolo
 label0 = Label(text = "Cerca e trova Files")
@@ -31,13 +35,18 @@ label.config(bg='lightgreen', padx=10)
 ricerca = Entry(text = "")
 ricerca.place(x=190, y=40, width=200, height=20)
 
+risultato = Label(font="calibri", height="35", width="70")
+
 #inserisco bottone per avvio ricerca
-button = Button(text = "ricerca", command = esegui_ricerca)
+button = Button(text = "ricerca", command = view_ricerca)
 button.place(x=200, y=80, width=75, height=35)
 
+button = Button(text = "chiudi", command = window.destroy)
+button.place(x=290, y=80, width=75, height=35)
 
 
 
 
 
+#start per il programma 
 window.mainloop() 
